af09b6c25588462861489f5419c45523fa5b99c0
edmondxiexie <xiejy36@gmail.com>
2018-04-18 02:39:21 -0700

    Fix footer style

--- frontend/components/footer/Footer.js
+++ frontend/components/footer/Footer.js
@@ -23,10 +23,41 @@ export default class Footer extends Component {
           </div>
 
           <div className="contact">
+            <div className="contact-social">
+              <label>Social Link</label>
+              <div>
+                <a
+                  href="https://github.com/edmondxiexie/edmondhome"
+                  target="_blank"
+                >
+                  <i className="fa fa-github" aria-hidden="true" />
+                  <span>Github</span>
+                </a>
+              </div>
+              <div>
+                <a
+                  href="https://www.linkedin.com/in/edmondxie/"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                  <span>Linkedin</span>
+                </a>
+              </div>
+              <div>
+                <a
+                  href="https://www.facebook.com/profile.php?id=100009769728183"
+                  target="_blank"
+                >
+                  <i className="fa fa-facebook-square" aria-hidden="true" />
+                  <span>Facebook</span>
+                </a>
+              </div>
+            </div>
+
             <div className="contact-traditional">
               <label>Contact Author</label>
               <div>
-                <i className="fa fa-user" aria-hidden="true" />
+                <i className="fa fa-user-o" aria-hidden="true" />
                 <span>Jiayi Edmond Xie</span>
               </div>
               <div>
@@ -34,26 +65,9 @@ export default class Footer extends Component {
                 <span>412-932-0155</span>
               </div>
               <div>
-                <i className="fa fa-envelope" aria-hidden="true" />
+                <i className="fa fa-envelope-o" aria-hidden="true" />
                 <span>xiejy36@gmail.com</span>
               </div>
-            </div>
-            <div className="contact-social">
-              <a
-                href="https://github.com/edmondxiexie/edmondhome"
-                target="_blank"
-              >
-                <i className="fa fa-github" aria-hidden="true" />
-              </a>
-              <a href="https://www.linkedin.com/in/edmondxie/" target="_blank">
-                <i className="fa fa-linkedin" aria-hidden="true" />
-              </a>
-              <a
-                href="https://www.facebook.com/profile.php?id=100009769728183"
-                target="_blank"
-              >
-                <i className="fa fa-facebook" aria-hidden="true" />
-              </a>
             </div>
           </div>
         </div>
