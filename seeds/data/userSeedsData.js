5a6d4de71799e6796b6328e645aa1a26e5522301
edmondxiexie <xiejy36@gmail.com>
2018-04-23 02:24:13 -0700

    Remove some cartoon fake avatar

--- seeds/data/userSeedsData.js
+++ seeds/data/userSeedsData.js
@@ -5,8 +5,6 @@ module.exports = {
     "http://res.cloudinary.com/dqace5qmb/image/upload/v1523257081/Creative-Tail-People-wonder-women.svg.png",
     "http://res.cloudinary.com/dqace5qmb/image/upload/v1523257080/avatar-user-boy-389cd1eb1d503149-512x512.png",
     "http://res.cloudinary.com/dqace5qmb/image/upload/v1523257080/avatar-user-teacher-312a499a08079a12-512x512.png",
-    "http://res.cloudinary.com/dqace5qmb/image/upload/v1523257080/avatar-user-older-oldman-35243ea673999fc1-512x512.png",
-    "http://res.cloudinary.com/dqace5qmb/image/upload/v1522312537/avatar.jpg",
     "https://res.cloudinary.com/dqace5qmb/image/upload/v1524470799/51.jpg",
     "https://res.cloudinary.com/dqace5qmb/image/upload/v1524470799/8.jpg",
     "https://res.cloudinary.com/dqace5qmb/image/upload/v1524470799/31.jpg",
